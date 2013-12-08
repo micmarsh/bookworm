d3201b0f225f5c7a9ba52a079a8099577204d742
micmarsh <michael.marsh42@gmail.com>
2013-12-07 19:31:01 -0500

    update url before clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject bookworm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject bookworm "0.1.0"
+  :description "A Library for Reading Epubs in Clojure"
+  :url "https://github.com/micmarsh/bookworm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
